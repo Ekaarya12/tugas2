22124aa132aac60767500fe5bf919f2e12d47a6d
ekaaryasaputra <fatir12@gmail.com>
2023-08-29 07:48:00 +0700

    pagii

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-# Login-System-PHP-and-MYSQL
-Login System Using PHP and MYSQL
+# C.R.U.D Code Igniter v4
+Simple CRUD example using the new version of code igniter (4)
+
+## Getting start
+
+Make the clone of this repository
+```html
+$ git clone https://github.com/mathmed/crud-codeigniter4.git
+```
+
+## Setting up the database
+
+First of all, you need to configure your database database (eg. MySQL, MariaDB). Import the `crud-codeigniter4/crudci4.sql` located in the project root in your database.  
+After do this, set your database config in  `crud-codeigniter4/app/config/Database.php`.  
+
+## Starting the server
+
+Check if you have the latest php version installed, if you have, go to project root and execute  
+
+```html
+$ php spark serve
+```
+
+your server will start on port 8080.  
+
+## Accessing
+
+To access the project, type in your browser  
+```html
+http://localhost:8080
+```
+
+### Code Igniter 4 docs
+
+For details on how to use Code Igniter 4, visit the [documentation](https://codeigniter4.github.io/userguide/).
 
 
---
+++ app/Views/pages/users.php
@@ -0,0 +1,146 @@
+<?= $session->getFlashdata("message") ?>
+
+<script>
+    $(document).ready(() => {
+
+        /* config fields to update/delete a user */
+        edit = (user) => {
+
+            $("#text-edit-user").text("Edit " + ($(user).attr("username")));
+            $("#edit-name").val(($(user).attr("username")));
+            $("#edit-age").val(($(user).attr("userage")));
+            $("#edit-email").val(($(user).attr("useremail")));
+            $("#edit-id").val(($(user).attr("userid")));
+            $("#btn-delete").attr("href", "/users/delete/" + $(user).attr("userid"));
+
+        }
+
+    })
+</script>
+<!-- LOKASI TEXT PENCARIAN -->
+<form action="" method="get">
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="cari" aria-label="cari" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">cari</button>
+    </div>
+
+</form>
+<form method="post" action="/users/create">
+    <div class="modal" tabindex="-1" role="dialog" id="add-user">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title light-dark "><b>Add new User</b></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="light-dark">Name</label>
+                        <input class="form-control" name="name" required placeholder="Ex: John Wick">
+                    </div>
+                    <div class="form-group">
+                        <label class="light-dark">Age</label>
+                        <input class="form-control" name="age" required type="number">
+                    </div>
+                    <div class="form-group">
+                        <label class="light-dark">E-mail</label>
+                        <input class="form-control" name="email" required type="email" placeholder="Example: john@wick.com">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><b>Cancel</b></button>
+                    <button type="submit" class="btn btn-success"><b>Insert <i class="fas fa-check-double icon"></i></b></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<form method="post" action="/users/update">
+    <input type="hidden" name="id" id="edit-id">
+    <div class="modal" tabindex="-1" role="dialog" id="edit-user">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title light-dark" id="text-edit-user"><b></b></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="light-dark">Name</label>
+                        <input class="form-control" name="name" required placeholder="Ex: John Wick" id="edit-name">
+                    </div>
+                    <div class="form-group">
+                        <label class="light-dark">Age</label>
+                        <input class="form-control" name="age" required type="number" id="edit-age">
+                    </div>
+                    <div class="form-group">
+                        <label class="light-dark">E-mail</label>
+                        <input class="form-control" name="email" required type="email" placeholder="Example: john@wick.com" id="edit-email">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><b>Cancel</b></button>
+                    <a id="btn-delete"><button type="button" class="btn btn-danger"><b>Delete <i class="fas fa-info icon"></i></b></button></a>
+                    <button type="submit" class="btn btn-success"><b>Update <i class="fas fa-check-double icon"></i></b></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<div class="margin-top row">
+    <div class="col-md-3 center">
+        <h3 class="light-dark">Users</h3>
+    </div>
+    <div class="col-md-3 center">
+        <button class="btn btn-info" data-toggle="modal" data-target="#add-user"><b>Add user <i class="fas fa-plus icon"></i></b></button>
+    </div>
+</div>
+
+<div class="margin-top">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>E-mail</th>
+                <th>Edit</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach ($users as $user) : ?>
+                <tr>
+                    <td><?= $user->name ?></td>
+                    <td><?= $user->age ?></td>
+                    <td><?= $user->email ?></td>
+                    <td>
+                        <button userage="<?= $user->age ?>" useremail="<?= $user->email ?>" username="<?= $user->name ?>" userid="<?= $user->id ?>" onclick="edit(this)" data-toggle="modal" data-target="#edit-user" class="btn btn-sm btn-primary"><b><i class="fas fa-bars"></i></b></button>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
